e6925713fe845feabeb924d1754f4ef4261c621f
Marrek Nožka <nozka@spseol.cz>
2017-11-20 12:16:20 +0100

    hashovani hesel

--- webface/views.py
+++ webface/views.py
@@ -4,6 +4,7 @@
 ############################################################################
 from flask import (render_template, request, flash,
                    redirect, session, url_for)
+from werkzeug.security import check_password_hash
 from webface import app
 ############################################################################
 
@@ -18,12 +19,18 @@ def login():
     return render_template('login.html')
 
 
+pwhash = {}
+pwhash['marek'] = 'pbkdf2:sha1:1000$XQdCKTo3$3deeb4dc4c670b48f6183d4884ca99cda534294c'
+pwhash['tonda'] = 'pbkdf2:sha1:1000$aBDfFHU2$2ae95982c428af53851fe5d0205e8f5623bd10ab'
+pwhash['katka'] = 'pbkdf2:sha1:1000$2zq2mD5B$66bcce17e76eb41597432d8d603f7cdd6b460043'
+
+
 @app.route('/login/', methods=['POST'])
 def login_post():
     jmeno = request.form.get('jmeno')
     heslo = request.form.get('heslo')
     print(jmeno, heslo)
-    if jmeno == 'marek' and heslo == 'fixa':
+    if check_password_hash(str(pwhash.get(jmeno)), heslo):
         session['jmeno'] = jmeno
         flash('Úspěšně jsi se přihlásil.', 'zelena')
         return redirect(url_for('index'))
@@ -38,9 +45,13 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/number/<i>')
-def number(i):
-    return render_template('number.html', number=i)
+@app.route('/tajne/')
+def tajne():
+    if 'jmeno' in session:
+        return render_template('tajne.html')
+    else:
+        flash('Tato stránka je jen pro přihlášené.', 'oranzova')
+        return redirect(url_for('login'))
 
 
 @app.errorhandler(404)
